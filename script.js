12c34f8aef186883ad47fe80e31cc5e5322b8c3e
Guilherme Lobato <38756533+gclobato@users.noreply.github.com>
2024-04-18 10:29:09 -0300

    .

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ Quagga.init({
     },
     decoder: {
         readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
+<<<<<<< HEAD
+    }
+}, function(err) {
+    if (err) {
+        console.log(err);
+        return
+=======
     }
 }, function(err) {
     if (err) {
@@ -23,6 +30,34 @@ Quagga.init({
 
 Quagga.onDetected(function(result) {
     var code = result.codeResult.code;
+    if (validateNumber(code)) {
+        alert("Número detectado: " + code);
+        addToTable(code);
+    } else {
+        alert("Número inválido.");
+>>>>>>> parent of f038f19 (up)
+    }
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+});
+
+<<<<<<< HEAD
+Quagga.onDetected(function(result) {
+    var code = result.codeResult.code;
     alert("Número detectado: " + code);
     // Você pode fazer o que quiser com o número detectado aqui, como enviar para um servidor ou exibir na tela.
 });
+=======
+function validateNumber(number) {
+    // Verificar se o número corresponde ao padrão 000.000-0
+    var regex = /^\d{3}\.\d{3}-\d$/;
+    return regex.test(number);
+}
+
+function addToTable(number) {
+    var table = document.getElementById("numbersTable").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.rows.length);
+    var cell = newRow.insertCell(0);
+    cell.innerHTML = number;
+}
+>>>>>>> parent of f038f19 (up)
